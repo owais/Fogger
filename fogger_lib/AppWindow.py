7aaf43176f6fd996f1de23af7f4156d4d3a4e241
Owais Lone <hello@owaislone.org>
2012-07-01 01:52:47 +0530

    Made some changes to app loading to adjust to /opt packaging

--- fogger_lib/AppWindow.py
+++ fogger_lib/AppWindow.py
@@ -1,16 +1,16 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 # Copyright (C) 2012 Owais Lone <hello@owaislone.org>
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3, as published 
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along 
+#
+# You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
@@ -62,8 +62,8 @@ class AppWindow(Gtk.Window):
         self.AboutDialog = None # class
 
 
-        self.settings = Gio.Settings("net.launchpad.fogger")
-        self.settings.connect('changed', self.on_preferences_changed)
+        #self.settings = Gio.Settings("net.launchpad.fogger")
+        #self.settings.connect('changed', self.on_preferences_changed)
         self.is_popup = False
         self.popups = []
 
@@ -133,8 +133,10 @@ class AppWindow(Gtk.Window):
             Gtk.main_quit()
         return True
 
+    '''
     def on_preferences_changed(self, settings, key, data=None):
         logger.debug('preference changed: %s = %s' % (key, str(settings.get_value(key))))
+    '''
 
     def on_preferences_dialog_destroyed(self, widget, data=None):
         '''only affects gui
