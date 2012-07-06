9f06bc46ba064aeb01c470fdc0e1e266bfb8c28a
Owais Lone <hello@owaislone.org>
2012-07-06 14:31:55 +0530

    commit before release

--- fogger/FoggerWindow.py
+++ fogger/FoggerWindow.py
@@ -1,16 +1,16 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 # Copyright (C) 2012 Owais Lone <hello@owaislone.org>
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 3, as published
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
 # by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranties of
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
 # PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
+# 
+# You should have received a copy of the GNU General Public License along 
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
@@ -139,11 +139,13 @@ class FoggerWindow(Window):
     def set_loading_url(self, loading):
         if loading:
             self.spinner.show()
-            self.create_button.set_sensitive(False)
+            #self.create_button.set_sensitive(False)
+            self.create_button.hide()
             self.url.set_sensitive(False)
         else:
             self.spinner.hide()
-            self.create_button.set_sensitive(True)
+            self.create_button.show()
+            #self.create_button.set_sensitive(True)
             self.url.set_sensitive(True)
 
     def set_error_message(self, message):
