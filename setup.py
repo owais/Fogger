3bb4a4a95855ec86b8df78387f0c5c8c5adfaf38
Owais Lone <hello@owaislone.org>
2012-07-07 13:22:26 +0530

    Updated package descriptions

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 # Copyright (C) 2012 Owais Lone <hello@owaislone.org>
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3, as published 
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along 
+#
+# You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
@@ -89,10 +89,10 @@ DistUtilsExtra.auto.setup(
     license='GPL-3',
     author='Owais Lone',
     author_email='hello@owaislone.org',
-    #description='UI for managing …',
-    #long_description='Here a longer description',
+    description='Fogger lets you create sandboxed web apps that behave like '\
+            'regular desktop applications.',
+    long_description='Fogger acts as a bridge between webapps and the linux desktop. It exposes an easy to use JS API to webapps which can be used to interact with the host desktop environment.',
     url='https://launchpad.net/fogger',
     cmdclass={'install': InstallAndUpdateDataDirectory},
-    scripts=['bin/fogger', 'bin/fogger-lens']
     )
 
