fd73f0dbbf31abdbbc00416613fd2beae08e7535
Owais Lone <hello@owaislone.org>
2012-07-08 01:01:25 +0530

    commit before release

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='fogger',
-    version='12.07.21',
+    version='12.07.22',
     license='GPL-3',
     author='Owais Lone',
     author_email='hello@owaislone.org',
