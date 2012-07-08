24c9cae8ae69e50b1b8e108dbc3e6ac6742d20d0
Owais Lone <hello@owaislone.org>
2012-07-08 17:32:30 +0530

    Fixed a bug in command line interface

--- fogger/__init__.py
+++ fogger/__init__.py
@@ -1,16 +1,16 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 # Copyright (C) 2012 Owais Lone <hello@owaislone.org>
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3, as published 
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along 
+#
+# You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 import optparse
@@ -38,7 +38,7 @@ def parse_options():
         "-r", "--remove", action="store", dest="remove",
         help=_("Remove a fogapp"))
     parser.add_option(
-        "-R", "--remove-all", action="store", dest="remove_all",
+        "-R", "--remove-all", action="store_true", dest="remove_all",
         help=_("Remove all fogapp"))
     parser.add_option(
         "-c", "--clean", action="store_true", dest="clean",
