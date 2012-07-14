b796f66c2de08ccbe825bd480ad25e7571622ef4
Owais Lone <hello@owaislone.org>
2012-07-14 22:33:59 +0530

    Added Licence to fogger_lib/DB.py

--- fogger_lib/DB.py
+++ fogger_lib/DB.py
@@ -1,3 +1,18 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2012 Owais Lone <hello@owaislone.org>
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
 from os import path as op
 import sqlite3 as sq
 import logging
