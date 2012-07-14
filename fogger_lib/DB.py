bedc1373b44bd11fcee6cbb35261ba31ad0014b2
Owais Lone <hello@owaislone.org>
2012-07-14 21:44:36 +0530

    * Added a failsafe class which emulates any attribute resolution of function call   This can be used in place of classes which should not break the app in case they fail themselves.
      The idea:
    
         class RealClass(object):
            def __new__(cls):
                try:
                    # Initialize here and return new instance
                except:
                    # return FailSafe instance which will log errors but will fail silently without breaking eecution of the real program

--- fogger_lib/DB.py
+++ fogger_lib/DB.py
@@ -1,7 +1,11 @@
 from os import path as op
 import sqlite3 as sq
+import logging
+logger = logging.getLogger('fogger_lib')
 
 from gi.repository import GLib
+
+from . helpers import get_or_create_directory
 
 DB_PATH = op.join(GLib.get_user_cache_dir(), 'fogger')
 DEFAULT_WIDTH = 1024
@@ -9,17 +13,52 @@ DEFAULT_HEIGHT = 768
 DEFAULT_STATE = False # True if maximized
 
 
-class StateDB(object):
-    _con = None
-    def __init__(self):
-        db_path = op.join(DB_PATH, 'state.db')
-        self._con = sq.connect(db_path)
-        cursor = self._con.cursor()
-        cursor.execute('CREATE TABLE IF NOT EXISTS WINDOW_STATE(id string primary key, width int, height int, maximized bool)')
-        self._con.commit()
+class FailSafeClass(object):
+    '''
+    This class acts as a dummy object and can be used in place of any other
+    class. Used to safely fallback to default or dummy values in case
+    non-mission critical objects fail to operate
+    '''
+    DEFAULT_VALUES = {
+        'load_state': (DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_STATE),
+        'fetchone': None,
+    }
 
-    def __del__(self):
-        self._con.close()
+    def __init__(self, name=None):
+       self.name = name
+
+    def __getattr__(self, attr):
+        logger.debug('FailSafe class emulated %s' % attr)
+        return FailSafeClass(attr)
+
+    def __call__(self, *args, **kwargs):
+        if self.name in self.DEFAULT_VALUES:
+            value = self.DEFAULT_VALUES[self.name]
+            logger.debug('FailSafe class returned %s for %s' % (value, self.name))
+            return value
+        else:
+            logger.debug('FailSafe emulated call for  %s' % self.name)
+            return FailSafeClass()
+
+
+class StateDB(object):
+    _con = FailSafeClass()
+
+    def __new__(cls):
+        #db_path = op.join(get_or_create_directory(DB_PATH), 'state.db')
+        #db_path = op.join(DB_PATH, 'state.db')
+        db_path = op.join('/home/owais/Desktop/kuta/', 'state.db')
+
+        try:
+            if (cls._con == None) or isinstance(cls._con, FailSafeClass):
+                cls._con = sq.connect(db_path)
+                cursor = cls._con.cursor()
+                cursor.execute('CREATE TABLE IF NOT EXISTS WINDOW_STATE(id string primary key, width int, height int, maximized bool)')
+                cls._con.commit()
+                return object.__new__(cls)
+        except sq.OperationalError:
+            logger.error('Could not connect to the database: %s' % db_path)
+            return FailSafeClass()
 
     def load_state(self, uuid):
         cursor = self._con.cursor()
@@ -35,10 +74,8 @@ class StateDB(object):
         cursor.execute("REPLACE INTO WINDOW_STATE (id, width, height, maximized) VALUES (?,?,?,?)", values)
         self._con.commit()
 
-
 state_db = None
 def get_state_db():
     global state_db
     state_db = state_db or StateDB()
     return state_db
-
