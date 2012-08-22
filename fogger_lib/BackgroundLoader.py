1a5cfd5bcfedd2e0202a5dc2a67081e4bfbaac4d
Owais Lone <hello@owaislone.org>
2012-08-22 14:36:42 +0530

    Changed chameleonic colour fetching a bit

--- fogger_lib/BackgroundLoader.py
+++ fogger_lib/BackgroundLoader.py
@@ -7,12 +7,13 @@ from . helpers import get_media_file
 def get_chameleonic_pixbuf_from_svg(filename):
     # Note: Experimental!!!!
     # TODO: Fix or Remove completely
+    data = open(get_media_file(filename, '/'), 'r').read()
     display = Display()
     screen = display.screen()
     atom = display.get_atom('_GNOME_BACKGROUND_REPRESENTATIVE_COLORS')
-    result = screen.root.get_property(atom, Xatom.STRING, 1, 3).value
-    color = 'rgb(%s, %s, %s)' % tuple(result.partition(')')[0].split(','))
-    data = open(get_media_file(filename, '/'), 'r').read()
-    data = data.replace('#000000', color)
+    result = screen.root.get_property(atom, Xatom.STRING, 1, 3)
+    if result:
+        color = result.value.strip('\x00')
+        data = data.replace('#000000', color)
     h = Rsvg.Handle.new_from_data(data)
     return h.get_pixbuf()
