b4607b0b51ac8e12f4eece94c1ce28990df9c2ec
Owais Lone <hello@owaislone.org>
2013-09-30 11:31:48 +0530

    Fixed chameleonic cloud colour

--- fogger_lib/BackgroundLoader.py
+++ fogger_lib/BackgroundLoader.py
@@ -1,20 +1,32 @@
-from gi.repository import Rsvg
+from gi.repository import Rsvg, Gdk
 from Xlib import Xatom
 from Xlib.display import Display
 
 from . helpers import get_media_file
 
-def get_chameleonic_pixbuf_from_svg(filename):
-    # Note: Experimental!!!!
-    # TODO: Fix or Remove completely
-    data = open(get_media_file(filename, '/'), 'r').read()
+
+def __patch_svg_data__(data):
     display = Display()
     screen = display.screen()
     atom = display.get_atom('_GNOME_BACKGROUND_REPRESENTATIVE_COLORS')
     result = screen.root.get_property(atom, Xatom.STRING, 0, 100)
     if result:
-        print result.value
         color = result.value.strip('\x00')
+        rgba = Gdk.RGBA()
+        rgba.parse(color)
+        color = rgba.to_color().to_string()
         data = data.replace('#000000', color)
+    return data
+
+
+def get_chameleonic_pixbuf_from_svg(filename):
+    # Note: Experimental!!!!
+    # TODO: Fix or Remove completely
+    data = open(get_media_file(filename, '/'), 'r').read()
+    try:
+        data = __patch_svg_data__(data)
+    except:
+        # this shouldn't crash the app. fail silently
+        pass
     h = Rsvg.Handle.new_from_data(data)
     return h.get_pixbuf()
